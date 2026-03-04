401266e58a160dfc739ffe17e5db0672bc7dd479
Rajarshi Tummala <tummalarajarshi1@gmail.com>
2026-03-03 19:25:26 -0500

    added sound pma

--- Assets/PMA Scripts/SoundPMA.cs
+++ Assets/PMA Scripts/SoundPMA.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SoundPMA : MonoBehaviour
 {
+    [Header("Sound to play for the attack.")]
+    public AudioClip clip;
+    [Header("Test the attack.")]
+    public bool test;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,10 @@ public class SoundPMA : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(test)
+        {
+            test = false;
+            AudioSource.PlayClipAtPoint(clip, new Vector3(0, 0, 0), 1f);
+        }
     }
 }
